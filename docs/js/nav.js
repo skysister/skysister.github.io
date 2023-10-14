bb134c474b40efc866db9ca709d7d7f04b0923c7
Maru Skyborne <mariesfine@gmail.com>
2023-10-14 13:36:23 -0500

    Attempt to load the nav on document ready.

--- docs/js/nav.js
+++ docs/js/nav.js
@@ -1,6 +1,10 @@
 const nav = {
     onDocumentReady: function () {
-        
+        nav.loadPrimary();
+    },
+
+    loadPrimary: function (container = "#nav-primary") {
+        $(container).load("/template/nav-primary.html");
     }
 };
 
