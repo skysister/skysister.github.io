8f5323ba3b9edc3790b5773ec7b95c2af06944a6
Maru Skyborne <mariesfine@gmail.com>
2023-10-15 10:50:45 -0500

    Add code to load footer nav

--- docs/js/nav.js
+++ docs/js/nav.js
@@ -1,10 +1,15 @@
 const nav = {
     onDocumentReady: function () {
         nav.loadPrimary();
+        nav.loadFooter();
     },
 
     loadPrimary: function (container = "#nav-primary") {
         $(container).load("/template/nav-primary.html");
+    },
+
+    loadFooter: function (container = "#nav-footer") {
+        $(container).load("/template/nav-footer.html");
     }
 };
 
