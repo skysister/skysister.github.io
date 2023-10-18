0cbbebc7d8df900d1d4d55b33882a1d1c4afff30
Maru Skyborne <mariesfine@gmail.com>
2023-10-18 17:16:11 -0500

    Stub in and link the pool JS

---
+++ docs/oremon/pool.js
@@ -0,0 +1,23 @@
+var pool = {
+    init: function () {
+        console.log("pool.init()");
+        OnClick.install("pool"); // attaches click handlers
+
+        pool.data = localStorage.getItem("oremon-pool-data");
+        if (pool.data == null) {
+            pool.noData();
+        } else {
+            pool.foundData();
+        }
+    },
+
+    noData: function() {
+        console.log("pool.noData()");
+        oremon.loadTemplate("#welcome", user.variables(user.current));
+        oremon.loadTemplate("#new-pool", {}, false); // don't empty
+    },
+
+    foundData: function() {
+        console.log("pool.foundData()");
+    }
+};
