2cee7a99793037f8086132256f2ffa6450c542a3
Maru Skyborne <mariesfine@gmail.com>
2023-10-18 20:40:10 -0500

    Add a default site date format

--- docs/js/site.js
+++ docs/js/site.js
@@ -1,4 +1,6 @@
 var site = {
+    dateFormat: "dddd, MMMM D, YYYY @ h:mm:ss a",
+    
     onDocumentReady: function() {
         console.log("site.onDocumentReady()");
         OnClick.install("site"); // attaches click handlers
