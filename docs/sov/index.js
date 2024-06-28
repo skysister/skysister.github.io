a8acbda88c3c0252ba57f09a618530c73d69f757
Maru Skyborne <mariesfine@gmail.com>
2024-06-28 18:58:09 -0500

    Start sov object

---
+++ docs/sov/index.js
@@ -0,0 +1,77 @@
+var sov = {
+    map: { names: [] },
+
+    onDocumentReady: function () {
+        console.log("sov.onDocumentReady()");
+        OnClick.install("sov"); // attaches click handlers
+        sov.log("Ready");
+    },
+
+    onRun: function () {
+        console.log("sov.run()");
+        sov.getMap();
+    },
+
+    getMap: function () {
+        sov.log("sov.getMap()");
+        var endpoint = "https://esi.evetech.net/latest/sovereignty/map/?datasource=tranquility";
+        sov.log(endpoint);
+
+        fetch(endpoint)
+            .then(response => response.json())
+            .then(result => {
+                sov.map.original = result;
+                sov.log("Received " + sov.map.original.length + " systems.");
+                sov.filterMap();
+                // sov.getNames(sov.systemIDs());
+                const systemIDs = sov.systemIDs();
+                const maxEntries = 1000;
+                for (i = 0; i < systemIDs.length; i += maxEntries) {
+                    const maxSystemIDs = systemIDs.slice(i, i + maxEntries);
+                    sov.getNames(maxSystemIDs);
+                }
+                sov.log("Names has " + sov.map.names.length + " systems.");
+            });
+    },
+
+    getNames: function (systemIDs) {
+        sov.log("sov.getNames() " + systemIDs.length);
+
+        var endpoint = "https://esi.evetech.net/latest/universe/names/?datasource=tranquility";
+
+        var body = JSON.stringify(systemIDs);
+        var headers = {
+            "accept": "application/json",
+            "Content-Type": "application/json",
+        };
+        var method = "post";
+
+        fetch(endpoint, { body, headers, method })
+            .then(response => response.json())
+            .then(result => {
+                sov.log("Received " + result.length + " names.");
+                sov.map.names.push(...result);
+            });
+    },
+
+    filterMap: function () {
+        sov.log("sov.filterMap()");
+        sov.map.withAlliance = sov.map.original.filter(
+            s => Object.keys(s).includes("alliance_id")
+        );
+        sov.log("Filtered to " + sov.map.withAlliance.length + " systems.");
+    },
+
+    systemIDs: function () {
+        sov.log("systemIDs()");
+        return sov.map.withAlliance.map(system => system.system_id);
+    },
+
+    log: function (message) {
+        $("#log").append(message + "\n");
+    },
+
+    endOfObject: true
+};
+
+$(sov.onDocumentReady);
