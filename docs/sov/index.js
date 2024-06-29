2e64ef740db9524fab49813c8a31223f81292f43
Maru Skyborne <mariesfine@gmail.com>
2024-06-29 06:35:17 -0500

    Sort methods into rough order of execution and document each

--- docs/sov/index.js
+++ docs/sov/index.js
@@ -12,34 +12,71 @@ var sov = {
         sov.getMap();
     },
 
+    /**
+     * getMap
+     * populates sov.map.original from ESI /sovereignty/map/
+     * 
+     * calls sov.filterMap() to reduce map to those with alliances
+     * calls sov.systemIDs() to construct array for getting system names
+     * calls sov.getSystemNames()
+     */
     getMap: function () {
         sov.log("sov.getMap()");
         const endpoint = "https://esi.evetech.net/latest/sovereignty/map/?datasource=tranquility";
-        sov.log(endpoint);
 
+        // call ESI /sovereignty/map/
         fetch(endpoint)
             .then(response => response.json())
             .then(result => {
                 sov.map.original = result;
-                sov.log("Received " + sov.map.original.length + " systems.");
+                sov.log("- sov.map.original has " + sov.map.original.length + " systems.");
                 sov.filterMap();
-                // sov.getNames(sov.systemIDs());
+
+                // get system names 1,000 at a time
                 const systemIDs = sov.systemIDs();
                 const maxEntries = 1000;
                 const requests = [];
                 for (i = 0; i < systemIDs.length; i += maxEntries) {
                     const maxSystemIDs = systemIDs.slice(i, i + maxEntries);
-                    requests.push(sov.getNames(maxSystemIDs));
+                    requests.push(sov.getSystemNames(maxSystemIDs));
                 }
+
+                // continue once all the requests are complete
                 Promise.all(requests)
                     .then(() => {
-                        sov.log("Names has " + sov.map.names.length + " systems.");
+                        sov.log("- sov.map.names has " + sov.map.names.length + " systems.");
+                        sov.mapNames();
                     });
             });
     },
 
-    getNames: function (systemIDs) {
-        sov.log("sov.getNames() " + systemIDs.length);
+    /**
+     * filterMap
+     * populates sov.map.withAlliance with systems that have an alliance_id
+     */
+    filterMap: function () {
+        sov.log("sov.filterMap()");
+        sov.map.withAlliance = sov.map.original.filter(
+            s => Object.keys(s).includes("alliance_id")
+        );
+        sov.log("- sov.map.withAlliance has " + sov.map.withAlliance.length + " systems.");
+    },
+
+    /**
+     * systemIDs
+     * returns an array for getting system names
+     */
+    systemIDs: function () {
+        sov.log("sov.systemIDs()");
+        return sov.map.withAlliance.map(system => system.system_id);
+    },
+
+    /**
+     * getSystemNames
+     * calls ESI /universe/names/ to retrieve system names in bulk
+     */
+    getSystemNames: function (systemIDs) {
+        sov.log("sov.getSystemNames() " + systemIDs.length);
 
         const endpoint = "https://esi.evetech.net/latest/universe/names/?datasource=tranquility";
 
@@ -58,24 +95,13 @@ var sov = {
             });
     },
 
-    filterMap: function () {
-        sov.log("sov.filterMap()");
-        sov.map.withAlliance = sov.map.original.filter(
-            s => Object.keys(s).includes("alliance_id")
-        );
-        sov.log("Filtered to " + sov.map.withAlliance.length + " systems.");
-    },
-
-    systemIDs: function () {
-        sov.log("sov.systemIDs()");
-        return sov.map.withAlliance.map(system => system.system_id);
+    mapNames: function() {
+        sov.log("sov.mapNames()");
     },
 
     log: function (message) {
         $("#log").append(message + "\n");
-    },
-
-    endOfObject: true
+    }
 };
 
 $(sov.onDocumentReady);
