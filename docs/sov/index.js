30ee683018caeca69e3fb6faba2b06e40c2eb1f9
Maru Skyborne <mariesfine@gmail.com>
2024-06-29 09:25:29 -0500

    Remove previous methods

--- docs/sov/index.js
+++ docs/sov/index.js
@@ -9,7 +9,7 @@ var sov = {
 
     onRun: function () {
         sov.log("onRun()");
-        // sov.getMap();
+
         sov.esiSovMap()
             .then(() => { sov.metrics(sov.map.original, "original:", "systems"); })
             .then(sov.filterSystems)
@@ -18,6 +18,7 @@ var sov = {
             .then(() => { sov.metrics(sov.map.names, "name:", "systems"); })
             .then(sov.alliances)
             .then(() => { sov.metrics(Object.keys(sov.map.alliance), "alliance:", "alliances"); })
+            .then(sov.combineMapData)
     },
 
     metrics: function (theArray, name, items) {
@@ -53,6 +54,15 @@ var sov = {
             requests.push(sov.esiUniverseNames(chunk));
         }
         return Promise.all(requests);
+    },
+
+    /**
+     * systemIDs
+     * returns an array for getting system names
+     */
+    systemIDs: function () {
+        sov.log("systemIDs()");
+        return sov.map.withAlliance.map(system => system.system_id);
     },
 
     esiUniverseNames: function (systemIDs) {
@@ -91,96 +101,11 @@ var sov = {
     },
 
     esiAlliance: function (alliance_id) {
-        console.log("sov.esiAlliance(" + alliance_id + ")");
         const endpoint = "https://esi.evetech.net/latest/alliances/" + alliance_id + "/?datasource=tranquility";
 
         return fetch(endpoint)
             .then(response => response.json())
             .then(result => { sov.map.alliance[alliance_id] = result; });
-    },
-
-    /**
-     * getMap
-     * populates sov.map.original from ESI /sovereignty/map/
-     * 
-     * calls sov.filterMap() to reduce map to those with alliances
-     * calls sov.systemIDs() to construct array for getting system names
-     * calls sov.getSystemNames()
-     * calls sov.alliances()
-     */
-    getMap: function () {
-        sov.log("sov.getMap()");
-        const endpoint = "https://esi.evetech.net/latest/sovereignty/map/?datasource=tranquility";
-
-        // call ESI /sovereignty/map/
-        fetch(endpoint)
-            .then(response => response.json())
-            .then(result => {
-                sov.map.original = result;
-                sov.log("- sov.map.original has " + sov.map.original.length + " systems.");
-                sov.filterMap();
-
-                // get system names 1,000 at a time
-                const systemIDs = sov.systemIDs();
-                const maxEntries = 1000;
-                const requests = [];
-                for (i = 0; i < systemIDs.length; i += maxEntries) {
-                    const maxSystemIDs = systemIDs.slice(i, i + maxEntries);
-                    requests.push(sov.getSystemNames(maxSystemIDs));
-                }
-
-                // continue once all the requests are complete
-                Promise.all(requests)
-                    .then(() => {
-                        sov.log("- sov.map.names has " + sov.map.names.length + " systems.");
-                        sov.alliances();
-                    });
-            });
-    },
-
-    /**
-     * filterMap
-     * populates sov.map.withAlliance with systems that have an alliance_id
-     */
-    filterMap: function () {
-        sov.log("sov.filterMap()");
-        sov.map.withAlliance = sov.map.original.filter(
-            s => Object.keys(s).includes("alliance_id")
-        );
-        sov.log("- sov.map.withAlliance has " + sov.map.withAlliance.length + " systems.");
-    },
-
-    /**
-     * systemIDs
-     * returns an array for getting system names
-     */
-    systemIDs: function () {
-        sov.log("systemIDs()");
-        return sov.map.withAlliance.map(system => system.system_id);
-    },
-
-    /**
-     * getSystemNames
-     * calls ESI /universe/names/ to retrieve system names in bulk
-     */
-    getSystemNames: function (systemIDs) {
-        sov.log("sov.getSystemNames() " + systemIDs.length);
-
-        const endpoint = "https://esi.evetech.net/latest/universe/names/?datasource=tranquility";
-
-        const body = JSON.stringify(systemIDs);
-        const headers = {
-            "accept": "application/json",
-            "Content-Type": "application/json",
-        };
-        const method = "post";
-
-        return fetch(endpoint, { body, headers, method })
-            .then(response => response.json())
-            .then(result => {
-                sov.log("Received " + result.length + " names.");
-                sov.map.names.push(...result);
-            });
     },
 
     /**
